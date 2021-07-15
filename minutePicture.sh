a044e8cc4778f43ea27dfbac5a4ecba5f43d5f23
mattsmiths <matthewsmith01@g.harvard.edu>
2021-07-15 00:45:41 -0500

    Update minutePicture.sh

--- minutePicture.sh
+++ minutePicture.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-raspistill --nopreview -ss 3000 -t 55000 --timelapse 1000 -o /mnt/autopolls/$DATE%06d.jpg
+HOURCHK=$(date +"%H")
+
+if $([ $(($HOURCHECK)) -gt 5 ]) && $([ $(($HOURCHECK)) -lt 22 ])
+  raspistill --nopreview -ss 3000 -t 55000 --timelapse 1000 -o /mnt/autopolls/$DATE%06d.jpg
+fi
+
+#raspistill --nopreview -ss 3000 -t 55000 --timelapse 1000 -o /mnt/autopolls/$DATE%06d.jpg
 #raspivid -w 4056 -h 3040 -cd MJPEG -o /mnt/autopolls/$DATE.mjpeg --nopreview -t 55000 -fps 1 -ss 3000
